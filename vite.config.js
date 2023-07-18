79f2900c144f46686ba046c5e1f2030af3a3bba8
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-07-18 01:11:27 -0300

    gh14

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import copy from 'rollup-plugin-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [{ src: 'img/**/*', dest: 'dist/img' }],
+      verbose: true,
+    }),
+  ],
   base: '/TopOne/',
 });
