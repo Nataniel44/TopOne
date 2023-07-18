39780ce3c055d56d888e945e45cc0cb565b9197a
Nataniel44 <126529831+Nataniel44@users.noreply.github.com>
2023-07-18 00:54:04 -0300

    gh11

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Page1 from './page1';
 import Menu from './Menu';
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" element={<Page1 />} />
-        <Route path="/menu" element={<Menu />} />
-      </Switch>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Page1 />} />
+      <Route path="/menu" element={<Menu />} />
+    </Routes>
   );
 };
 
