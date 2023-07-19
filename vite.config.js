1bf5e3e26b1b3345bd65ad44f40b6631355aeffb
nataniel44 <josenatanielsoto@gmail.com>
2023-07-19 13:33:38 -0300

    ajustes2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import copy from 'rollup-plugin-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    copy({
-      targets: [{ src: 'img/**/*', dest: 'dist/img' }],
-      verbose: true,
-    }),
-  ],
-  base: '/TopOne/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    assetsInlineLimit: 0,
+  },
 });
